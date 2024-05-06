f573841d32d360a4ebe0ab17bbb26abc58e2ace2
thiago <thiago@tvidal.uk>
2024-05-06 18:34:35 +0100

    format code style

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,19 @@
 #define TIMER_INTERVAL      666
 #define BLINK_DELAY         66
 
+#define TEMP_COUNT          6
 #define TEMP_MIN            22.0f
 #define TEMP_MAX            30.0f
 #define TEMP_LIMIT          50.0f
 
 #define isTempReading       digitalRead(LED_BUILTIN)
-#define temp_read           (temp_sum / temp_count)
+#define temp_read           (temp_sum / TEMP_COUNT)
 #define fan_read            digitalRead(FAN_SW)
 
 #define setFanPWM(pwm)      OCR2B = (uint8_t)(FAN_PWM_HIGH*(pwm+0.5f))
 
-const char *ON = "ON";
-const char *OFF = "OFF";
+auto* ON = "ON";
+auto* OFF = "OFF";
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -36,16 +37,16 @@ SimpleTimer timer(TIMER_INTERVAL);
 
 OneButton button(BTN_SW);
 
-const uint8_t temp_count = 6;
 uint8_t temp_index = 0;
 float temp_sum = 0.0f;
-float temp_data[temp_count];
+float temp_data[TEMP_COUNT];
 
 float fan_pwm = 0.0f;
 
 bool button_override = false;
 
-static void buttonPress() {
+static
+void buttonPress() {
   button_override = !button_override;
 }
 
@@ -86,7 +87,7 @@ void setupTempSensor() {
   sensors.setWaitForConversion(false);
   sensors.setResolution(SENSOR_RESOLUTION);
 
-  for (int i = 0; i < temp_count; ++i) {
+  for (int i = 0; i < TEMP_COUNT; ++i) {
     temp_data[i] = 0.0f;
   }
   temp_sum = 0.0f;
@@ -119,7 +120,7 @@ float readTemperature() {
       temp_sum -= temp_data[temp_index];
       temp_data[temp_index] = temp;
       temp_sum += temp;
-      temp_index = (temp_index + 1) % temp_count;
+      temp_index = (temp_index + 1) % TEMP_COUNT;
       return temp_read;
     }
   }
@@ -139,14 +140,14 @@ void setFan(const float temp) {
 void printTemperatureStatus(const float temp) {
   Serial.print("temp[");
   Serial.print(temp_data[0], 1);
-  for (int i = 1; i < temp_count; ++i) {
+  for (int i = 1; i < TEMP_COUNT; ++i) {
     Serial.print(", ");
     Serial.print(temp_data[i], 1);
   }
   Serial.print("] sum=");
   Serial.print(temp_sum, 1);
   Serial.print("/");
-  Serial.print(temp_count);
+  Serial.print(TEMP_COUNT);
   Serial.print("=");
   Serial.print(temp);
   Serial.print("°C");
