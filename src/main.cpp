c0895c985078d24c935fcb9861d890f2a685ea84
thiago <thiago@tvidal.uk>
2024-09-21 18:54:07 +0100

    decrease temp limits

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@
 #define BLINK_DELAY         66
 
 #define TEMP_COUNT          6
-#define TEMP_MIN            26.0f
-#define TEMP_MAX            40.0f
+#define TEMP_MIN            24.0f
+#define TEMP_MAX            28.0f
 #define TEMP_LIMIT          55.0f
 
 #define isTempReading       digitalRead(LED_BUILTIN)
